d00e0066b43eacc301050215196db18dc6ca3482
zhangsan <zhangsan@gmail.com>
2026-09-18 09:42:04 +0800

    ad

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 window.XIANGPAN_CONFIG = {
   mode: 'cloud',
   // 部署后改成你的项目：https://<PROJECT_REF>.supabase.co/functions/v1/api
-  cloudApiBase: 'https://urwzkpqflmkzwssadvpw.supabase.co/functions/v1/api',
+  cloudApiBase: 'https://urwzkpqflmkzwssadvpw.supabase.co/functions/v1',
   localApiBase: 'http://127.0.0.1:5038',
   localOrigin: 'http://127.0.0.1:5038'
 };
